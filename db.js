536bd838f060e98820035d68a524d050f24ea94b
Serifenuraslan <serifenuraslan705@gmail.com>
2026-04-03 18:15:59 +0300

    hata yakalayici eklendi

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres", // PgAdmin resminde gördük, bu doğru ✅
-  password: "1234", // <-- BURAYA KENDİ ŞİFRENİ YAZ (Unuttuysan 12345 veya root dene)
-  host: "localhost",
-  port: 5432,
-  database: "kampus_db", // <-- İşte aradığımız isim bu! 🎯
+  // Aşağıdaki tırnak içine Neon'dan kopyaladığın uzun linki yapıştır!
+  connectionString:
+    "postgresql://neondb_owner:npg_fNRw27gixpUt@ep-orange-water-an5acdnp-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
