44c146dc980b8b65d12c5ef5e3c8bac4065fb0d0
Serifenuraslan <serifenuraslan705@gmail.com>
2026-05-14 02:03:07 +0300

    Yeni rotalar eklendi

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // Aşağıdaki tırnak içine Neon'dan kopyaladığın uzun linki yapıştır!
+  // BURADAKİ TIRNAK İÇİNE NEON'DAN KOPYALADIĞIN O UZUN LİNKİ YAPIŞTIR!
   connectionString:
     "postgresql://neondb_owner:npg_fNRw27gixpUt@ep-orange-water-an5acdnp-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: {
